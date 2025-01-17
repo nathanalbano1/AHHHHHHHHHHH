7bae79c54c66a98ed40bc4af44c5d0ea77dd5e45
nathanalbano1 <MallariAb21@gmail.com>
2025-01-16 19:59:41 -0800

    Add files via upload

---
+++ NNN.py
@@ -0,0 +1,14 @@
+def main():
+    number = get_number()
+    meow(3)
+
+
+def get_number():
+    while True: 
+        n = int(input("What's n?"))
+        if n > 0:
+            return n 
+
+def meow(n):
+    for _ in range (n):
+        print("hi")
